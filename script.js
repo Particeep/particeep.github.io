b26b161b6a82d8a0c2cad64078c9c57c5f96432b
Alexandre Lissac <alex36330@gmail.com>
2022-02-21 10:39:09 +0100

    Fix CDN JQuery

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".search").on("keyup", function () {
-        var search = this.value.toLowerCase();
-        $(".card").show().filter(function () {
-            return $(".title-card", this).text().toLowerCase().indexOf(search) < 0;
-        }).hide();        
-    });
-})
